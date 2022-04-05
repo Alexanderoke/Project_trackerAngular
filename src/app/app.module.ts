19567886a25cf70640032bccb042ef72d1284000
FidelisMomanyi <fidelismomanyi001@gmail.com>
2022-04-05 11:09:31 +0300

    sign-up, sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,21 +10,47 @@ import { TrackerService } from './tracker.service';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
+// Firebase services + environment module
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { AuthService } from './shared/services/auth.service';
+    
 @NgModule({
   declarations: [
     AppComponent,
     ProjectComponent,
     UplodProjectComponent,
-    ViewProjectsComponent
+    ViewProjectsComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [TrackerService],
+  providers: [TrackerService], 
+  // providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
